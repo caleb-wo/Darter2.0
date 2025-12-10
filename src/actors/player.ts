996afb2cd43e24443fb8962c4c8cb2d74ffa5114
calebwolfe <caleb-wolfe@gmx.com>
2025-12-09 18:04:30 -0700

    Adding comments for class pres just in case

--- src/actors/player.ts
+++ src/actors/player.ts
@@ -4,28 +4,46 @@ import { Config } from "../scripts/config";
 export class Player extends ex.Actor {
     health = 10;
     isGrounded = false;
+    isWallSlidingLeft = false;
+    isWallSlidingRight = false;
 
     constructor() {
+        // SETUP- w/ actor constructor.
         super({
             name: "Darter",
             pos: Config.PlayerStartPos,
             width: 16,
             height: 16,
-            color: ex.Color.Orange
+            color: ex.Color.Orange,
         });
         this.body.collisionType = ex.CollisionType.Active;
     }
 
     override onInitialize(engine: ex.Engine): void {
+        // SETUP- prepare state mutation to be triggered on collisions
+        this.on('collisionstart', (ev) => {
+            // Wall contact logic
+            if (ev.side === ex.Side.Left) {
+                this.isWallSlidingLeft = true;
+            }
+            if (ev.side === ex.Side.Right) {
+                this.isWallSlidingRight = true;
+            }
+            if (ev.side === ex.Side.Bottom) {
+                this.isGrounded = true;
+            }
+        });
+
         this.on('collisionend', (ev) => {
             if (ev.side === ex.Side.Bottom) {
                 this.isGrounded = false;
             }
-        });
-        
-        this.on('collisionstart', (ev) => {
-            if (ev.side === ex.Side.Bottom) {
-                this.isGrounded = true;
+            // Wall separation logic
+            if (ev.side === ex.Side.Left) {
+                this.isWallSlidingLeft = false;
+            }
+            if (ev.side === ex.Side.Right) {
+                this.isWallSlidingRight = false;
             }
         });
     }
@@ -53,51 +71,3 @@ export class Player extends ex.Actor {
         this.vel.x = ex.clamp(this.vel.x, Config.PlayerMinVelocity, Config.PlayerMaxVelocity);
     }
 }
-
-
-// import * as ex from "excalibur";
-// import { Config } from "../scripts/config"
-
-// export class Player extends ex.Actor {
-//     health = 10;
-//     jumping = false;
-
-//     constructor() {
-//         super({
-//             name: "Darter",
-//             pos: Config.PlayerStartPos,
-//             width: 16,
-//             height: 16,
-//             color: ex.Color.Orange
-//         });
-//         this.body.collisionType = ex.CollisionType.Active;
-//     }
-
-//     private isJumpInputActive(engine: ex.Engine): boolean {
-//         return engine.input.keyboard.isHeld(ex.Keys.Space) 
-//                || engine.input.keyboard.isHeld(ex.Keys.W);
-//     }
-
-//     override onPostUpdate(engine: ex.Engine): void {
-//         this.acc.x = 0; 
-
-//         if (engine.input.keyboard.isHeld(ex.Keys.A) || engine.input.keyboard.isHeld(ex.Keys.ArrowLeft)) {
-//             this.acc.x = -Config.PlayerAcceleration;
-//         }
-
-//         if (engine.input.keyboard.isHeld(ex.Keys.D) || engine.input.keyboard.isHeld(ex.Keys.ArrowRight)) {
-//             // Set acceleration to the right
-//             this.acc.x = Config.PlayerAcceleration;
-//         }
-
-//         if (!this.jumping && this.isJumpInputActive(engine)) {
-//             this.vel.y = Config.PlayerJumpVelocity; // Use config, not -800
-//             this.jumping = true;
-//         }
-//         if (!this.isJumpInputActive(engine)) {
-//             this.jumping = false;
-//         }
-
-//         this.vel.x = ex.clamp(this.vel.x, Config.PlayerMinVelocity, Config.PlayerMaxVelocity);
-//     }
-// }
